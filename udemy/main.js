6f830b980f540d4e75303e8704f5784c17f660e4
Saurabh Mirajkar <mirajkar46@gmail.com>
2017-10-13 10:48:42 +0530

    Building interest in Web Development

---
+++ udemy/main.js
@@ -0,0 +1,22 @@
+//Numeric type
+var a = 4;
+//String type
+var b = "character";
+var c = " is equal to ";
+var mycar = "Lambo";
+var mycolor = "blue";
+//Boolean type
+var d = true; 
+
+if (!d) {
+	console.log('d is false');
+}else{
+	console.log('d is true');
+}
+
+console.log(mycar +' '+ mycolor);
+console.log(c + c + c);
+
+var output = document.getElementById('output');
+console.log(output);
+
